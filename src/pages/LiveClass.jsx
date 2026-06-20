5503b91a212a4ce20161e06fdc17e52ec986c136
Bipin Kumar <developer69bipin70@gmail.com>
2026-06-20 10:46:11 +0530

    Added YouTube Live

--- src/pages/LiveClass.jsx
+++ src/pages/LiveClass.jsx
@@ -45,10 +45,14 @@ export default function LiveClass() {
       <div className="live-class-container">
         <div className="video-wrapper">
           <iframe
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-            title="Live Class Stream"
+            width="100%"
+            height="600"
+            src="https://www.youtube.com/embed/cliDYHcCBHE"
+            title="Live Class"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
-          />
+          ></iframe>
         </div>
 
         <div className="live-info">
